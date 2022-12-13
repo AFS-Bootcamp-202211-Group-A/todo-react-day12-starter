d9f73107c8572bbbbd0cabc1f186f710e941e009
heinrich-oocl <117629815+heinrich-oocl@users.noreply.github.com>
2022-12-13 15:59:55 +0800

    refactor: optimise variable passing

--- src/features/TodoItem.js
+++ src/features/TodoItem.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function TodoItem(props) {
-  return <div>{props.todo}</div>;
+
+  var {id, text, done} = props.todo;
+
+
+    return (
+    <div style={{textDecoration: props.todo.done && "line-through"}}>
+        {text}
+      <button>
+        x
+      </button>
+    </div>
+    );
 }
--- src/features/TodoList.js
+++ src/features/TodoList.js
@@ -1,15 +1,13 @@
 import React from "react";
 import TodoGenerator from "./TodoGenerator";
 import TodoGroup from "./TodoGroup";
-import { useSelector } from "react-redux";
+
 
 
 export default function TodoList() {
-  const todos = useSelector((state) => state.todo.todoItems);
-  console.log(todos);
   return (
     <div>
-      <TodoGroup todos={todos} />
+      <TodoGroup/>
       <TodoGenerator/>
     </div>
   );
