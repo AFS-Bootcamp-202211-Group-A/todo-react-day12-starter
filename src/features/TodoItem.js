af3af4ce64b24afbf76c1c6d19ef2b96500b1742
Alan Wan <alan.wan@oocl.com>
2022-12-13 17:41:47 +0800

    feat: finish add, delete, line-through features

--- src/features/TodoItem.js
+++ src/features/TodoItem.js
@@ -1,5 +1,34 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleTodo, removeTodo } from "./todoSlice";
 
 export default function TodoItem(props) {
-  return <div>{props.todo}</div>;
+  const { id, text, done } = props.todo;
+  const dispatch = useDispatch();
+  const onDoneTodo = () => {
+    dispatch(toggleTodo(id));
+  };
+  const onRemoveTodo = () => {
+    dispatch(removeTodo(id));
+  };
+  return (
+    <div
+      style={{
+        border: "1px solid black",
+        width: "30%",
+        margin: "auto",
+        padding: "10px",
+        textAlign: "left",
+      }}
+    >
+      <span onClick={onDoneTodo}>
+        {done ? (
+          <span style={{ textDecoration: "line-through" }}>{text}</span>
+        ) : (
+          <span>{text}</span>
+        )}
+      </span>
+      <button onClick={onRemoveTodo}>x</button>
+    </div>
+  );
 }
