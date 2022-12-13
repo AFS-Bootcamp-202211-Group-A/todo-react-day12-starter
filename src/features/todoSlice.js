5ac47c009d2ec5a6e3cfaca93f2ded5c74940616
Marie Chow <chowma3@oocl.com>
2022-12-13 15:53:20 +0800

    feat: add demo data

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -2,13 +2,17 @@ import { createSlice, current } from "@reduxjs/toolkit";
 
 const todoSlice = createSlice({
   name: "todoList",
-  initialState: [],
+  initialState: [
+    {
+      id: 1,
+      text: "demo",
+      done: false,
+    },
+  ],
   reducers: {
     addTodo: (state, action) => {
       console.log(current(state));
-      console.log("sizeValue:", action.payload);
-      // state = [...state, action.payload];
-      console.log(state.length);
+      console.log("todoValue:", action.payload);
       const todoObject = {
         id: state.length + 1,
         text: action.payload,
