756d8808f66a6b1bd5eb5f8682a08444c9a72101
kwokth4 <kwokth4@oocl.com>
2022-12-13 16:42:15 +0800

    feat: toggle todoitem

--- src/features/TodoItem.js
+++ src/features/TodoItem.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TodoItem(props) {
+  const [toggle, setToggle] = useState(true);
+  const handleClick = (event) => {
+    setToggle(!toggle);
+    console.log(toggle);
+    if(!toggle){
+      event.target.style.removeProperty('text-decoration');
+    }
+    else{
+      event.target.style.setProperty('text-decoration', 'line-through');
+    }
+  };
   return (
-    <div className="itemBorder">
-      <p className="todoItem">{props.todo.text}</p>
+    <div className="itemBorder" onClick={handleClick}>
+    
+        <p className="todoItem">{props.todo.text}</p> 
+          
     </div>
   )
 }
